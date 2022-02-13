8fa4b6fc0d4f83e06b6c2ea19fdb4e664d6de986
Hugo <hugolacerdar@gmail.com>
2022-02-13 18:18:37 -0300

    chore: enumerables examples, part 2

--- scores.rb
+++ scores.rb
@@ -13,3 +13,16 @@ scores.select! {|s| s > 70}
 p scores
 scores.reject! {|s| s.even?}
 p scores
+
+scores = [83, 71, 92, 64, 98, 87, 75, 69]
+scores_doubled = scores.map {|s| s * 2}
+p scores_doubled
+
+class_sum = scores.reduce(0,:+)
+puts "Total score: #{class_sum}"
+
+evens, odds = scores.partition {|s| s.even?}
+puts "Evens:"
+p evens
+puts "Odds:"
+p odds
