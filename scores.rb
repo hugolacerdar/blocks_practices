376f0bdf70534124e7c64b7b50f63b6f741b6e66
Hugo <hugolacerdar@gmail.com>
2022-02-13 10:09:17 -0300

    chore: add enumerables examples

---
+++ scores.rb
@@ -0,0 +1,9 @@
+scores = [83, 71, 92, 64, 98, 87, 75, 69]
+
+highscores = scores.select {|s| s > 80}
+p highscores
+
+lowscores = scores.reject {|s| s > 80}
+p lowscores
+
+puts scores.detect {|s| s < 70}
