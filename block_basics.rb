9c21c61d0bfd63ae7a35685f3dca1df25695a2d1
Hugo <hugolacerdar@gmail.com>
2022-02-13 09:31:34 -0300

    chore: blocks basic examples

---
+++ block_basics.rb
@@ -0,0 +1,18 @@
+5.times do
+    puts "situp"
+    puts "pushup"
+    puts "chinup"
+  end
+  
+  5.times do |count|
+    puts "#{count} situp"
+    puts "#{count} pushup"
+    puts "#{count} chinup"
+  end
+  
+  1.upto(5) do |count|
+    puts "#{count} situp"
+    puts "#{count} pushup"
+    puts "#{count} chinup"
+  end
+  
