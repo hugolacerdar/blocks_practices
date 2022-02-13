71dc0e4c4012a3ef11bdcb5b5c26007d6188f92c
Hugo <hugolacerdar@gmail.com>
2022-02-13 18:18:23 -0300

    chore: enumerables, part 2

--- flyer-status.rb
+++ flyer-status.rb
@@ -31,3 +31,21 @@ puts flyers.any? {|f| f.status == :platinum}
 puts "\n"
 first_bronze_flyer = flyers.detect {|f| f.status == :bronze}
 puts first_bronze_flyer
+puts "\n"
+
+platinum_flyers, non_platinum_flyers = flyers.partition {|f| f.status == :platinum}
+puts "Platinum flyers:"
+puts platinum_flyers
+puts "Non-platinum flyers:"
+puts non_platinum_flyers
+puts "\n"
+tagged_flyers = flyers.map {|f| "#{f.name} (#{f.status.upcase})"}
+p tagged_flyers
+puts "\n"
+totals_in_kms = flyers.map { |f| f.miles_flown * 1.6 }
+p totals_in_kms
+puts "\n"
+miles_sum = flyers.reduce(0) {|acc, f| acc + f.miles_flown}
+puts "Total miles: #{miles_sum}"
+kms_sum = totals_in_kms.reduce(0, :+)
+puts "Total kilometers: #{kms_sum}"
