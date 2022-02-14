e6532749e83f22604b139474a9158de193fee2ec
Hugo <hugolacerdar@gmail.com>
2022-02-14 10:59:15 -0300

    chore: open database driver class method

---
+++ database_driver.rb
@@ -0,0 +1,48 @@
+class DatabaseDriver
+  def initialize(database, user, password)
+    @database = database
+    @user = user
+    @password = password
+    @connected = false
+  end
+
+  def connect
+    # connects to database
+    @connected = true
+    puts "Connected to #{@database} as #{@user}."
+  end
+
+  def disconnect
+    # disconnects from database
+    puts "Disconnected."
+  end
+
+  def execute(sql)
+    raise "Not connected!" unless @connected
+    puts "Executing #{sql}..."
+    # executes SQL
+  end
+
+  def self.open(database, user, password)
+    driver = self.new("my_database", "admin", "secret")
+    driver.connect
+    return driver unless block_given?
+    begin
+      yield(driver)
+    rescue => exception
+      puts exception
+    ensure
+      driver.disconnect
+    end
+  end
+end
+
+DatabaseDriver.open("my_database", "admin", "secret") do |driver|
+  driver.execute("SELECT * FROM ORDERS")
+  driver.execute("SELECT * FROM USERS")
+end
+
+driver = DatabaseDriver.open("my_database", "admin", "secret")
+driver.execute("SELECT * FROM ORDERS")
+driver.execute("SELECT * FROM USERS")
+driver.disconnect
