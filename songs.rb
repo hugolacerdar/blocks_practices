6bb573b2fc99916eac8d4e75ce11a2c8d805bf53
Hugo <hugolacerdar@gmail.com>
2022-02-13 21:00:27 -0300

    refactor: mix in my enumerable module

--- songs.rb
+++ songs.rb
@@ -1,51 +1,54 @@
-class Song
-    attr_reader :name, :artist, :duration
-  
-    def initialize(name, artist, duration)
-      @name = name
-      @artist = artist
-      @duration = duration
-    end
-  
-    def play
-      puts "Playing '#{name}' by #{artist} (#{duration} mins)..."
-    end
+require_relative "my_enumerable"
 
-    def each_filename
-        extensions = [".mp3", ".wav", ".aac"]
-        filename = "#{@name}-#{@artist}".downcase.gsub(" ", "-")
-        extensions.each {|ext| yield filename + ext}
-    end
+class Song
+  attr_reader :name, :artist, :duration
+
+  def initialize(name, artist, duration)
+    @name = name
+    @artist = artist
+    @duration = duration
+  end
+
+  def play
+    puts "Playing '#{name}' by #{artist} (#{duration} mins)..."
+  end
+
+  def each_filename
+    extensions = [".mp3", ".wav", ".aac"]
+    filename = "#{@name}-#{@artist}".downcase.gsub(" ", "-")
+    extensions.each { |ext| yield filename + ext }
+  end
 end
 
 class Playlist
-    include Enumerable
+  # include Enumerable
+  include MyEnumerable
 
-    def initialize(name)
-      @name = name
-      @songs = []
-    end
-  
-    def add_song(song)
-      @songs << song
-    end
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
 
-    def each 
-        @songs.each {|song| yield song}
-    end
+  def add_song(song)
+    @songs << song
+  end
 
-    def each_tagline
-        @songs.each {|song| yield "#{song.name} - #{song.artist}"}
-    end
+  def each
+    @songs.each { |song| yield song }
+  end
 
-    def each_by_artist(artist)
-        @songs.each {|song| yield song if song.artist == artist}
-    end
+  def each_tagline
+    @songs.each { |song| yield "#{song.name} - #{song.artist}" }
+  end
 
-    def play_songs
-        puts "\nPlaying collection:"
-        each {|song| song.play}
-    end
+  def each_by_artist(artist)
+    @songs.each { |song| yield song if song.artist == artist }
+  end
+
+  def play_songs
+    puts "\nPlaying collection:"
+    each { |song| song.play }
+  end
 end
 
 song1 = Song.new("One", "Band One", 10)
@@ -61,20 +64,20 @@ playlist.each { |song| song.play }
 
 playlist.play_songs
 
-one_songs = playlist.select { |song| song.name =~ /One/ }
+one_songs = playlist.my_select { |song| song.name =~ /One/ }
 p one_songs
 
-non_one_songs = playlist.reject { |song| song.name =~ /One/ }
+non_one_songs = playlist.my_reject { |song| song.name =~ /One/ }
 p non_one_songs
 
-p playlist.any? { |song| song.artist == "Band Two" }
-p playlist.detect { |song| song.artist == "Band Two" }
+p playlist.my_any? { |song| song.artist == "Band Twos" }
+p playlist.my_detect { |song| song.artist == "Band One" }
 
-song_labels = playlist.map { |song| "#{song.name} - #{song.artist}" }
+song_labels = playlist.my_map { |song| "#{song.name} - #{song.artist}" }
 p song_labels
 
-total_duration = playlist.reduce(0) { |sum, song| sum + song.duration }
-p total_duration
+# total_duration = playlist.my_reduce(0) { |sum, song| sum + song.duration }
+# p total_duration
 
 playlist.each_tagline { |tagline| puts tagline }
 
